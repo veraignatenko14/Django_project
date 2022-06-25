97c1b1875aa593752bc889c11340f94931efc4c7
rik-code <topemven1234567890@gmail.com>
2022-06-25 16:34:18 +0300

    Adding pages

---
+++ hello/firstapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('Привет, user! Это мой сайт!')
+
+
+def about(request):
+    return HttpResponse('<h1>O нас</h1>')
